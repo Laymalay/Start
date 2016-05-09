3029cc96cfc551430bf97fbcc64b2bdeacf05f4f
Alina <laymalay@gmail.com>
2016-05-09 11:30:52 +0300

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -9,14 +9,15 @@ class Game {
 
 
     Game(float widht,float hight,string name);
+    void start();
     void control();
     void render();
     void update();
   private:
-
     Pet mycat_;
     sf::RenderWindow* window;
-    float const BUTTON_SHOWER_X = 0;
+
+    static constexpr float  BUTTON_SHOWER_X = 0;
     float const BUTTON_SHOWER_Y = 0;
     float const BUTTON_FOOD_X = 0;
     float const BUTTON_FOOD_Y = 0;
@@ -30,8 +31,10 @@ class Game {
     float const BUTTON_WORK_Y = 0;
     float const BUTTON_CURE_X = 0;
     float const BUTTON_CURE_Y = 0;
+    float const BUTTON_PLACEBO_X = 0;
+    float const BUTTON_PLACEBO_Y = 0;
 
-
+    void draw_button_placebo();
     void draw_button_shower();
     void draw_button_food();
     void draw_button_WC();
@@ -40,6 +43,36 @@ class Game {
     void draw_button_Work();
     void draw_button_Cure();
 
+    //////////////////////////////////////////
+    //              Condition               //
+    //////////////////////////////////////////
+    void show_condition(string condition);
+    void happy();
+    void good();
+    void so_so();
+    void sad();
+    void verysad();
+    void died();
+    void ill();
+    //////////////////////////////////////////
+    //                мысли                 //
+    //////////////////////////////////////////
+    void Cloud();
+
+    void hungry();
+    void need_wc();
+    void need_cure();
+
+    //////////////////////////////////////////
+    //              ACTION                  //
+    //////////////////////////////////////////
+    void eat();
+    void wc();
+    void sport();
+    void shower();
+    void work();
+    void play();
+    void cure();
 
 
 };
